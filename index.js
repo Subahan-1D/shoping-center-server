a89431cf40b71cfc6a37102283f6267bec1d00d8
subahan ali <subahanislam523@gmail.com>
2024-12-27 19:37:47 +0600

    added to by delete data

--- index.js
+++ index.js
@@ -132,10 +132,17 @@ async function run() {
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
-
+    // menu post
     app.post("/menu", async (req, res) => {
       const item = req.body;
       const result = await menuCollection.insertOne(item);
+      res.send(result);
+    });
+
+    app.delete("/menu/:id", verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.deleteOne(query);
       res.send(result);
     });
 
