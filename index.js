311025f4b240ab98e9c18ca021df6ada00aea7e9
subahan ali <subahanislam523@gmail.com>
2024-12-28 00:54:37 +0600

    added to by update product data

--- index.js
+++ index.js
@@ -139,10 +139,36 @@ async function run() {
       res.send(result);
     });
 
+    // update a menu data
+    app.get("/menu/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.findOne(query);
+      res.send(result);
+    });
+
     app.delete("/menu/:id", verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.patch("/menu/:id", async (req, res) => {
+      const item = req.body;
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          name: item.name,
+          price: item.price,
+          category: item.category,
+          quality: item.quality,
+          size: item.size,
+          image: item.image,
+        },
+      };
+      const result = await menuCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
 
