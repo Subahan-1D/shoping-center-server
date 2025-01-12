d4e023434920d212a951ab49f86d4b541f9d65c7
subahan ali <subahanislam523@gmail.com>
2025-01-12 18:33:25 +0600

    added to by payment history

--- index.js
+++ index.js
@@ -229,6 +229,17 @@ async function run() {
       res.send({ paymentResult, deleteResult });
     });
 
+    // payment history
+
+    app.get("/payments/:email", verifyToken, async (req, res) => {
+      const query = { email: req.params.email };
+      if (req.params.email !== req.decoded.email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      const result = await paymentCollection.find(query).toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
