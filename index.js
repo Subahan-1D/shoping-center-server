d4eca0ae1197b801b61f031ca281865cf6696936
subahan ali <subahanislam523@gmail.com>
2025-01-12 17:12:26 +0600

    carefully delete each item from the card

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ async function run() {
     const menuCollection = client.db("shopingCenter").collection("menu");
     const cartCollection = client.db("shopingCenter").collection("cart");
     const userCollection = client.db("shopingCenter").collection("users");
+    const paymentCollection = client.db("shopingCenter").collection("payments");
     // JSON WEB TOKEN RELATED API
     app.post("/jwt", async (req, res) => {
       const user = req.body;
@@ -201,6 +202,7 @@ async function run() {
     app.post("/create-payment-intent", async (req, res) => {
       const { price } = req.body;
       const amount = parseInt(price * 100);
+      console.log(amount, "inside the amount");
       const paymentIntent = await stripe.paymentIntents.create({
         amount: amount,
         currency: "usd",
@@ -209,6 +211,22 @@ async function run() {
       res.send({
         clientSecret: paymentIntent.client_secret,
       });
+    });
+
+    // payment for database
+    app.post("/payment", async (req, res) => {
+      const payment = req.body;
+      const paymentResult = await paymentCollection.insertOne(payment);
+
+      // carefully delete each item from the card
+      console.log("payment Information", payment);
+      const query = {
+        _id: {
+          $in: payment.cartIds.map((id) => new ObjectId(id)),
+        },
+      };
+      const deleteResult = await cartCollection.deleteMany(query);
+      res.send({ paymentResult, deleteResult });
     });
 
     // Send a ping to confirm a successful connection
